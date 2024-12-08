83eaff4c3e7951daf406c6099e79bb78e5dcb36b
Alyssa Ritchie <ar8756@princeton.edu>
2024-12-08 00:19:25 -0500

    added cone objects but dont work with turns

--- src/components/objects/ProceduralRoad/ProceduralRoad.js
+++ src/components/objects/ProceduralRoad/ProceduralRoad.js
@@ -1,14 +1,20 @@
 import { Group, Vector3 } from 'three';
+//import * as THREE from 'three';
 import RoadChunk from '../RoadChunk/RoadChunk.js';
 import RoadCorner from '../RoadCorner/RoadCorner.js';
+import Obstacle from '../Obstacle/Obstacle.js';
 
 class ProceduralRoad extends Group {
-    constructor(parent, {
-        segmentWidth = 25,
-        segmentLength = 100,
-        startSegments = 2,
-        fracTurns = 0.3,
-    } = {}) {
+    constructor(
+        parent,
+        {
+            segmentWidth = 25,
+            segmentLength = 100,
+            startSegments = 2,
+            fracTurns = 0.3,
+            obstacleSpawnProbability = 1,
+        } = {}
+    ) {
         super();
 
         this.state = {
@@ -19,8 +25,9 @@ class ProceduralRoad extends Group {
             segmentLength: segmentLength,
             fracTurns: fracTurns,
             startSegments: startSegments,
-            currentDirection: new Vector3(0, 0, 1), 
-            currentPosition: new Vector3(0, 0, 0)
+            currentDirection: new Vector3(0, 0, 1),
+            currentPosition: new Vector3(0, 0, 0),
+            obstacleSpawnProbability: obstacleSpawnProbability,
         };
 
         this.generateInitialRoad();
@@ -38,8 +45,10 @@ class ProceduralRoad extends Group {
         let segmentType = 'straight';
         if (!forceStraint) {
             const shouldTurn = Math.random() < this.state.fracTurns;
-            segmentType = shouldTurn 
-                ? (Math.random() < 0.5 ? 'turn-left' : 'turn-right') 
+            segmentType = shouldTurn
+                ? Math.random() < 0.5
+                    ? 'turn-left'
+                    : 'turn-right'
                 : 'straight';
         }
         let newCenter = new Vector3();
@@ -47,35 +56,72 @@ class ProceduralRoad extends Group {
 
         let newDirection = new Vector3(0, 0, 1);
         let nrCurSeg = this.state.roadSegments.length;
-        
+
         if (nrCurSeg > 0) {
             const lastPiece = this.state.roadSegments[nrCurSeg - 1];
             const oldCenter = lastPiece.state.center;
             const oldDirection = lastPiece.state.direction.clone();
             const axis = new Vector3(0, 1, 0);
-            newCorner = oldCenter.clone().add(oldDirection.clone().multiplyScalar(this.state.segmentLength/2 + this.state.segmentWidth*1/2).clone())
+            newCorner = oldCenter.clone().add(
+                oldDirection
+                    .clone()
+                    .multiplyScalar(
+                        this.state.segmentLength / 2 +
+                            (this.state.segmentWidth * 1) / 2
+                    )
+                    .clone()
+            );
 
             if (segmentType === 'straight') {
-
                 // newCenter = oldCenter.clone().add(oldDirection.clone().multiplyScalar(this.state.segmentLength/2 + this.state.segmentWidth*1/2).clone());
-                newCenter = oldCenter.clone().add(oldDirection.clone().multiplyScalar(this.state.segmentLength/2).clone());
+                newCenter = oldCenter.clone().add(
+                    oldDirection
+                        .clone()
+                        .multiplyScalar(this.state.segmentLength / 2)
+                        .clone()
+                );
 
                 newDirection = oldDirection.clone();
 
-                newCenter = newCenter.clone().add(newDirection.clone().multiplyScalar(this.state.segmentLength/2).clone()).clone()
-            } else { 
+                newCenter = newCenter
+                    .clone()
+                    .add(
+                        newDirection
+                            .clone()
+                            .multiplyScalar(this.state.segmentLength / 2)
+                            .clone()
+                    )
+                    .clone();
+            } else {
                 let angle = -Math.PI / 2;
                 if (segmentType === 'turn-left') {
                     angle = Math.PI / 2;
-                } 
-                newCenter = oldCenter.clone().add(oldDirection.clone().multiplyScalar(this.state.segmentLength*0.5 + this.state.segmentWidth*1/2).clone());
+                }
+                newCenter = oldCenter.clone().add(
+                    oldDirection
+                        .clone()
+                        .multiplyScalar(
+                            this.state.segmentLength * 0.5 +
+                                (this.state.segmentWidth * 1) / 2
+                        )
+                        .clone()
+                );
 
                 newDirection = oldDirection.clone().applyAxisAngle(axis, angle);
 
-                newCenter = newCenter.clone().add(newDirection.clone().multiplyScalar(this.state.segmentLength/2 + this.state.segmentWidth*1/2).clone()).clone()
+                newCenter = newCenter
+                    .clone()
+                    .add(
+                        newDirection
+                            .clone()
+                            .multiplyScalar(
+                                this.state.segmentLength / 2 +
+                                    (this.state.segmentWidth * 1) / 2
+                            )
+                            .clone()
+                    )
+                    .clone();
             }
-
-
         }
 
         // Create road segment
@@ -83,12 +129,16 @@ class ProceduralRoad extends Group {
             segmentWidth: this.state.segmentWidth,
             segmentLength: this.state.segmentLength,
             center: newCenter.clone(),
-            direction: newDirection.clone().normalize()
+            direction: newDirection.clone().normalize(),
         });
 
         // Add to scene and tracking
         this.add(roadSegment);
         this.state.roadSegments.push(roadSegment);
+
+        // spawn obstacles along this segment
+        this.spawnObstacles(newCenter, this.state.segmentWidth);
+
         if (segmentType === 'straight') {
             return roadSegment;
         }
@@ -113,14 +163,34 @@ class ProceduralRoad extends Group {
         return roadSegment;
     }
 
+    //todo: need to account for turns
+    spawnObstacles(roadCenter, roadWidth) {
+        const obstaclePosition = new Vector3(
+            roadCenter.x,
+            roadCenter.y + 2,
+            roadCenter.z
+        ); // Position above the road
+        new Obstacle(this.state.parent, obstaclePosition, roadWidth);
+    }
+
     update(timeStamp, student) {
         // generateNextRoadSegment
         const runnerPos = student.state.position;
         let nrCurSeg = this.state.roadSegments.length;
-        const lastPieceCenter = this.state.
-        roadSegments[nrCurSeg - 1].state.center;
-        let distance = runnerPos.manhattanDistanceTo(lastPieceCenter)
-        
+        const lastPieceCenter =
+            this.state.roadSegments[nrCurSeg - 1].state.center;
+
+        // manhattan distance doesn't work w/o vec3
+        if (!(runnerPos instanceof Vector3)) {
+            console.error('runnerPos is not a Vector3');
+            return;
+        }
+        if (!(lastPieceCenter instanceof Vector3)) {
+            console.error('lastPieceCenter is not a Vector3');
+            return;
+        }
+        let distance = runnerPos.manhattanDistanceTo(lastPieceCenter);
+
         if (distance < 700) {
             this.generateNextRoadSegment();
         }
--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -1,8 +1,9 @@
 import * as Dat from 'dat.gui';
-import { Scene, Color, Vector3} from 'three';
-import { RoadChunk, Student} from 'objects';
+import { Scene, Color, Vector3 } from 'three';
+import { RoadChunk, Student } from 'objects';
 import { BasicLights } from 'lights';
 import ProceduralRoad from '../objects/ProceduralRoad/ProceduralRoad';
+import Obstacle from '../objects/Obstacle/Obstacle';
 
 class SeedScene extends Scene {
     constructor() {
@@ -14,14 +15,16 @@ class SeedScene extends Scene {
             // gui: new Dat.GUI(), // Create GUI for scene
             rotationSpeed: 1,
             updateList: [],
-            roadLength: 1000,  
-            totalLength: 5000, 
-            student: null, 
-            roadChunk: null, 
-            lights: null, 
+            roadLength: 1000,
+            totalLength: 5000,
+            student: null,
+            obstacle: null,
+            roadChunk: null,
+            lights: null,
             roadWidth: 20,
             // cameraPos: new Vector3(),
             studentPos: new Vector3(),
+            obstaclePos: new Vector3(),
         };
 
         // Set background to a nice color
@@ -29,9 +32,20 @@ class SeedScene extends Scene {
 
         // Add meshes to scene
         this.state.roadChunk = new ProceduralRoad(this);
-        this.state.student = new Student(this, {roadWidth: this.state.roadWidth});
+        this.state.student = new Student(this, {
+            roadWidth: this.state.roadWidth,
+        });
+        this.state.obstacle = new Obstacle(this, {
+            roadWidth: this.state.roadWidth,
+        });
+
         this.state.lights = new BasicLights();
-        this.add(this.state.lights, this.state.roadChunk, this.state.student);
+        this.add(
+            this.state.lights,
+            this.state.roadChunk,
+            this.state.student,
+            this.state.obstacle
+        );
     }
 
     addToUpdateList(object) {
@@ -41,20 +55,20 @@ class SeedScene extends Scene {
     update(timeStamp) {
         const { updateList } = this.state;
         const stPos = this.state.student.state.position;
-        this.state.studentPos.set(
-            stPos.x,
-            stPos.y, 
-            stPos.z
-        )
+        const obsPos = this.state.obstacle.state.position;
+        this.state.studentPos.set(stPos.x, stPos.y, stPos.z);
+        this.state.obstaclePos.set(obsPos.x, obsPos.y, obsPos.z);
         // this.state.cameraPos.set(
         //     stPos.x,
-        //     stPos.y + 9, 
+        //     stPos.y + 9,
         //     stPos.z - 20
         // );
 
         for (const obj of updateList) {
             if (obj.constructor.name === 'ProceduralRoad') {
                 obj.update(timeStamp, this.state.student);
+                obj.update(timeStamp, this.state.obstacle);
+
                 // console.log('Updating ProceduralRoad');
             } else {
                 obj.update(timeStamp);
