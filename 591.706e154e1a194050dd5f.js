88e05bb1f6006119b725164d3a4a4fcc82147c92
Max Gonzalez Saez-Diez <gonzalezsaezdiez@gmail.com>
2024-12-13 13:14:30 -0500

    Updates

--- 591.706e154e1a194050dd5f.js
+++ 591.706e154e1a194050dd5f.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkprinceton_run=self.webpackChunkprinceton_run||[]).push([[591],{591:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=591.3d0b848bfd0cc42f4b71.js.map
+//# sourceMappingURL=591.706e154e1a194050dd5f.js.map
